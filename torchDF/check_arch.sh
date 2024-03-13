fab8ca9d752bd523bfb8258a553e40415491a9bd
a.korepanov <a.korepanov@skbkontur.ru>
2024-03-13 12:01:32 +0300

    remove checker

--- torchDF/check_arch.sh
+++
@@ -1,4 +0,0 @@
-export CUDA_VISIBLE_DEVICES=-1
-# poetry run python benchmark_nn_blocks.py
-poetry run python -m pytest -v --disable-warnings -k test_streaming_torch_with_streaming_torch_minimal
-poetry run python model_onnx_export.py --performance --simplify
